0678be048f3fcd6907974976c172086faec63f35
Eva Rodriguez <eva22rodri@gmail.com>
2019-07-02 10:59:33 -0500

    Basic idea of lemonade stand, need to fix repetition

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -6,7 +6,29 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the name of this lemonade stand?");
+            string lemonadeStandName = Console.ReadLine();
+         
+
+            Console.WriteLine("How much will your table cost?");
+            string tableCostResponse = Console.ReadLine();
+            decimal lemonadeStandTableCost = decimal.Parse(tableCostResponse);
+
+            Console.WriteLine("How much will the lemon cost per cup?");
+            string lemonCostResponse = Console.ReadLine();
+            decimal lemonadeStandLemonCostPerCup = decimal.Parse(lemonCostResponse);
+                
+            Console.WriteLine("What will be the price of a cup?");
+            string cupPriceResponse = Console.ReadLine();
+            decimal lemonadeStandPricePerCup = decimal.Parse(cupPriceResponse);
+
+            Console.WriteLine("Will you open a second lemonade stand?");
+            string secondStandResponse = Console.ReadLine();
+
+            if (secondStandResponse == "yes")
+            {
+                //Does something
+            }
         }
     }
 }
